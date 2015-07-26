807d4f625de745adbdbd8f2a670d1d1edb072c5a
MattiasZurkovic <mattias@zurkovic.com>
2015-07-26 10:10:09 +0200

    Make each page for dev studio

--- app/main.py
+++ app/main.py
@@ -8,36 +8,23 @@ from hashlib import sha1
 
 app = Flask(__name__)
 
-app.config.setdefault('TWEEPY_CONSUMER_KEY', 'aRahVNAuCVdWy5PGFjMoAIWui')
-app.config.setdefault('TWEEPY_CONSUMER_SECRET', 'fABUmGW1uV4pnlgTpwSx8KAxQdbVH6fz2le4dEW4e9wlnxmP2b')
-app.config.setdefault('TWEEPY_ACCESS_TOKEN_KEY', '2834176217-coE5CGfxIdniddoou1HOBcG3r4KVdVG2UzJQStS')
-app.config.setdefault('TWEEPY_ACCESS_TOKEN_SECRET', '3tfg6G4clDY42ie6wYekxf77xHGKCZjmWtUzIEqRTHqoW')
-
-@app.route('/')
-def index():
+@app.route('/valve')
+def valve():
     ##### RSS Feed Urls:
-    CSGO_URL = ['http://blog.counter-strike.net/index.php/feed/']
+    VALVE_URLS = ['http://blog.counter-strike.net/index.php/feed/',  # CSGO URL
+                  'http://blog.dota2.com/feed/',                     # DOTA URL
+                  'http://www.teamfortress.com/rss.xml',             # TF2 URL
+                  'http://www.sourcefilmmaker.com/rss.xml'           # Source Film Maker URL
+                  ]
 
-    DOTA_URL = ['http://blog.dota2.com/feed/']
+    ##### Valve Feed
+    valve_entries = []
+    for url in VALVE_URLS:
+        valve_entries.extend(feedparser.parse(url).entries)
 
-    TF2_URL = ['http://www.teamfortress.com/rss.xml']
+    valve_entries_sorted = sorted(valve_entries, key=lambda e: e.published_parsed, reverse=True)
 
-    SOURCEFILM_URL = ['http://www.sourcefilmmaker.com/rss.xml']
-
-    ##### CSGO Feed
-    csgo_entries = []
-    for url in CSGO_URL:
-        csgo_entries.extend(feedparser.parse(url).entries)
-
-    csgo_entries_sorted = sorted(
-        csgo_entries,
-        key=lambda e: e.published_parsed,
-        reverse=True)
-
-##### ===>
-##### ===>
-
-    return render_template('index.html', entries=csgo_entries_sorted)
+    return render_template('valve.html', entries=valve_entries_sorted)
 
 if __name__ == '__main__':
     app.run()
